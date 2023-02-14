ac9491c1692375967b0e215f73abac6a3a8dc5fd
Murat Altun <mraltun@gmail.com>
2023-02-14 15:09:08 +0300

    Added Navbar and scss. Added fonts and some dependencies

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='container'>
+        <div className='logo'>
+          {/* <Link to='/'>
+            <span className='text'>fiverr</span>
+          </Link> */}
+          <span className='dot'>.</span>
+        </div>
+        <div className='links'>
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default Navbar;
