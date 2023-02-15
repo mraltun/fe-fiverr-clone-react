a4d02b0b5525f9ede63fb2ee303afcbd7660e0b7
Murat Altun <mraltun@gmail.com>
2023-02-15 12:50:58 +0300

    Added Featured and scss

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Featured.scss";
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+      <div className='container'>
+        <div className='left'></div>
+        <div className='right'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Featured from "../../components/featured/Featured";
 import "./Home.scss";
 
 const Home = () => {
-  return <div className='home'>Home</div>;
+  return (
+    <div className='home'>
+      <Featured />
+    </div>
+  );
 };
 export default Home;
