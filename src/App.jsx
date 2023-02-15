3ca1851dfcac52b189f722cd6876854e2b1860c6
Murat Altun <mraltun@gmail.com>
2023-02-15 12:27:37 +0300

    Added RRD and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
 
 const App = () => {
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
   return (
     <div>
-      <Navbar />
+      <RouterProvider router={router} />
     </div>
   );
 };
